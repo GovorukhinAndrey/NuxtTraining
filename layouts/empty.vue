13d0e2eceba59730583795e02f961d07ee20b8fb
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-05-12 11:09:14 +0100

    Добавил новый layout

---
+++ layouts/empty.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="empty-layout">
+    <main>
+      <div class="container">
+        <nuxt />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+  .empty-layout {
+    display: flex;
+    justify-content: center;
+    padding-top: 5rem;
+  }
+</style>
