65c6eb53b680a060ccd60438931194a7fe4e0604
Andrey Govorukhin <andr.govorukhin@gmail.com>
2020-05-12 10:27:08 +0100

    Добавил страницу about

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>About page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque itaque sint, illo est magnam officia quod voluptatem ipsa dicta, doloremque vel laborum, quisquam optio pariatur sunt repudiandae at officiis eos.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque itaque sint, illo est magnam officia quod voluptatem ipsa dicta, doloremque vel laborum, quisquam optio pariatur sunt repudiandae at officiis eos.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque itaque sint, illo est magnam officia quod voluptatem ipsa dicta, doloremque vel laborum, quisquam optio pariatur sunt repudiandae at officiis eos.</p>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
